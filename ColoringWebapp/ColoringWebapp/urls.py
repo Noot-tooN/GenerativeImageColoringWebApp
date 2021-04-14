97174cdba16a9673d32f6622d61e92d425c22167
Aleksa <aleksa.novcic@rt-rk.com>
2021-04-14 11:58:49 +0200

    Added favicon

--- ColoringWebapp/ColoringWebapp/urls.py
+++ ColoringWebapp/ColoringWebapp/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import path, include
 from .views import *
 
 urlpatterns = [
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url("favicon.ico"))),
     path('', home_page, name="home_page"),
     path('colorization/', colorization_page, name="colorization_page"),
     path('user/', include('CustomUsers.urls')),
